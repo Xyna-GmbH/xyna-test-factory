723b7055bb0f2661f4fcfc2b7cdbdd44cd25c26d
johannesheucher-gip <107180330+johannesheucher-gip@users.noreply.github.com>
2023-12-14 17:45:41 +0100

    Update version.ts

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const TestFactoryName = 'Test Factory';
-export const TestFactoryVersion = '2.0.1';
+export const TestFactoryVersion = '2.1.0';
